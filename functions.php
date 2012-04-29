0f3b4acc8090f845b58641069e011ec40f802c31
Edward Caissie <edward.caissie@gmail.com>
2012-04-29 11:32:08 -0400

    Basic theme files

--- functions.php
+++ functions.php
@@ -15,3 +15,65 @@
 /** Call the initialization file to get things started */
 /** @noinspection PhpIncludeInspection - IDE commentary can be ignored */
 require_once( get_template_directory() . '/includes/opus-ignite.php' );
+
+
+/**
+ * Enqueue Comment Reply Script
+ *
+ * If the page being viewed is a single post/page; and, comments are open; and,
+ * threaded comments are turned on then enqueue the built-in comment-reply
+ * script.
+ *
+ * @package OpusPrimus
+ * @since   0.1
+ *
+ * @return  void
+ */
+if ( ! function_exists( 'opus_enqueue_comment_reply' ) ) {
+    function opus_enqueue_comment_reply() {
+        if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+            wp_enqueue_script( 'comment-reply' );
+        }
+    }
+}
+add_action( 'wp_enqueue_scripts', 'opus_enqueue_comment_reply' );
+
+
+if ( ! function_exists( 'opus_wp_title' ) ) {
+    /**
+     * Opus WP Title
+     *
+     * Utilizes the `wp_title` filter to add text to the default output
+     *
+     * @package     OpusPrimus
+     * @since       0.1
+     *
+     * @internal    Originally author by Edward Caissie
+     * @link        https://gist.github.com/1410493
+     *
+     * @param       string $old_title - default title text
+     * @param       string $sep - separator character
+     * @param       string $sep_location - left|right - separator placement in relationship to title
+     *
+     * @return      string - new title text
+     */
+    function opus_wp_title( $old_title, $sep, $sep_location ) {
+        global $page, $paged;
+        /** Set initial title text */
+        $opus_title_text = $old_title . get_bloginfo( 'name' );
+        /** Add wrapping spaces to separator character */
+        $sep = ' ' . $sep . ' ';
+
+        /** Add the blog description (tagline) for the home/front page */
+        $site_tagline = get_bloginfo( 'description', 'display' );
+        if ( $site_tagline && ( is_home() || is_front_page() ) )
+            $opus_title_text .= "$sep$site_tagline";
+
+        /** Add a page number if necessary */
+        if ( $paged >= 2 || $page >= 2 )
+            $opus_title_text .= $sep . sprintf( __( 'Page %s', 'opusprimus' ), max( $paged, $page ) );
+
+        return $opus_title_text;
+    }
+}
+add_filter( 'wp_title', 'opus_wp_title', 10, 3 );
--- header.php
+++ header.php
@@ -11,15 +11,12 @@
  * @author      Opus Primus <in.opus.primus@gmail.com>
  * @copyright   Copyright (c) 2012, Opus Primus
  */
-?><!DOCTYPE html>
-<!--[if IE 6]><html id="ie6" <?php language_attributes(); ?>><![endif]-->
-<!--[if IE 7]><html id="ie7" <?php language_attributes(); ?>><![endif]-->
-<!--[if IE 8]><html id="ie8" <?php language_attributes(); ?>><![endif]-->
-<!--[if !(IE 6) | !(IE 7) | !(IE 8)  ]><!--><html <?php language_attributes(); ?>><!--<![endif]-->
+?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>" />
-    <meta name="viewport" content="width=device-width" />
-    <title><?php wp_title(); ?></title>
+    <title><?php wp_title( '|', true, 'right' ); ?></title>
     <link rel="profile" href="http://gmpg.org/xfn/11" />
     <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
     <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
