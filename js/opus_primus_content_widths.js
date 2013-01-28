561835723a6e496283567b0900a8202dcad1c873
Edward Caissie <edward.caissie@gmail.com>
2013-01-28 11:49:17 -0500

    Add new script 'opus-primus-content-width.js' to better manage $content_width values

---
+++ js/opus_primus_content_widths.js
@@ -0,0 +1,40 @@
+/**
+ * Opus Primus Content Widths
+ * Overwrite the iframe width value with 100% instead of the default value set
+ * by $content_width global
+ *
+ * @package     OpusPrimus
+ * @since       0.1
+ *
+ * @author      Opus Primus <in.opus.primus@gmail.com>
+ * @copyright   Copyright (c) 2013, Opus Primus
+ *
+ * This file is part of Opus Primus.
+ *
+ * Opus Primus is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License version 2, as published by the
+ * Free Software Foundation.
+ *
+ * You may NOT assume that you can use any other version of the GPL.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to:
+ *
+ *      Free Software Foundation, Inc.
+ *      51 Franklin St, Fifth Floor
+ *      Boston, MA  02110-1301  USA
+ *
+ * The license for this software can also likely be found here:
+ * http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+jQuery( document ).ready( function( $ ) {
+    /** Note: $() will work as an alias for jQuery() inside of this function */
+
+    /** Change width to 100% from $content_width default */
+    $( '.format-audio p iframe' ).attr( 'width', '100%' );
+} );
