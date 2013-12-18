555c66e3a80585bd93207b7d6220481d4dcaf61f
Edward Caissie <edward.caissie@gmail.com>
2013-12-18 12:02:23 -0500

    * Re-format code with "new" WordPress-Cais CodeStyle Settings

--- 404.php
+++ 404.php
@@ -31,8 +31,8 @@
  * The license for this software can also likely be found here:
  * http://www.gnu.org/licenses/gpl-2.0.html
  *
- * @version 1.0.1
- * @date    February 21, 2013
+ * @version     1.0.1
+ * @date        February 21, 2013
  * Modified action hooks to more semantic naming convention:
  * `opus_<section>_<placement>`
  * Change classes from using underscores to using hyphens
@@ -44,88 +44,89 @@ get_header( '404' );
 /** Add empty hook before content */
 do_action( 'opus_content_before' ); ?>
 
-<div class="content-wrapper cf">
+	<div class="content-wrapper cf">
 
-    <?php
-    /** Add empty hook at top of the content */
-    do_action( 'opus_content_top' );
+		<?php
+		/** Add empty hook at top of the content */
+		do_action( 'opus_content_top' );
 
-    /** Open the necessary layout CSS classes */
-    echo $opus_structures->layout_open();
+		/** Open the necessary layout CSS classes */
+		echo $opus_structures->layout_open();
 
-    /** Add empty action before the_Loop */
-    do_action( 'opus_the_loop_before' ); ?>
+		/** Add empty action before the_Loop */
+		do_action( 'opus_the_loop_before' ); ?>
 
-    <div class="the-loop">
+		<div class="the-loop">
 
-        <?php
-        printf( '<h1 class="opus-404-title-text">%1$s</h1>',
-            apply_filters( 'opus_404_title_text', __( 'Something seems to have gone bust ...', 'opusprimus' ) )
-        );
+			<?php
+			printf( '<h1 class="opus-404-title-text">%1$s</h1>',
+				apply_filters( 'opus_404_title_text', __( 'Something seems to have gone bust ...', 'opusprimus' ) )
+			);
 
-        /** Use action hook to add image - see Structures class for hook call */
-        do_action( 'opus_404_image' );
+			/** Use action hook to add image - see Structures class for hook call */
+			do_action( 'opus_404_image' );
 
-        printf( '<p class="opus-404-message-text">%1$s</p>',
-            apply_filters( 'opus_404_message_text' , __( 'This is the 404 error page; we seemed to have lost what you are looking for.', 'opusprimus' ) )
-        );
+			printf( '<p class="opus-404-message-text">%1$s</p>',
+				apply_filters( 'opus_404_message_text', __( 'This is the 404 error page; we seemed to have lost what you are looking for.', 'opusprimus' ) )
+			);
 
-        printf( '<p class="opus-404-posts-text">%1$s</p>',
-            apply_filters( 'opus_404_posts_text', __( 'Were you looking for a recent post?', 'opusprimus' ) )
-        );
+			printf( '<p class="opus-404-posts-text">%1$s</p>',
+				apply_filters( 'opus_404_posts_text', __( 'Were you looking for a recent post?', 'opusprimus' ) )
+			);
 
-        /** Use the_widget to display a list of recent posts */
-        the_widget (
-            'WP_Widget_Recent_Posts',
-            $instance = array(
-                'title'     => __( '', 'opusprimus' ),
-                'number'    => '5',
-                'show_date' => true
-            )
-        );
+			/** Use the_widget to display a list of recent posts */
+			the_widget(
+				'WP_Widget_Recent_Posts',
+				$instance = array(
+					'title'     => __( '', 'opusprimus' ),
+					'number'    => '5',
+					'show_date' => true
+				)
+			);
 
-        /** Display links to archives */
-        printf( '<p class="opus-404-category-text">%1$s</p>',
-            apply_filters( 'opus_404_category_text', __( 'Maybe you were looking for one these categories ...', 'opusprimus' ) )
-        );
+			/** Display links to archives */
+			printf( '<p class="opus-404-category-text">%1$s</p>',
+				apply_filters( 'opus_404_category_text', __( 'Maybe you were looking for one these categories ...', 'opusprimus' ) )
+			);
 
-        /** Display a list of categories to choose from */
-        $opus_archives->categories_archive( array(
-            'orderby'       => 'count',
-            'order'         => 'desc',
-            'show_count'    => 1,
-            'hierarchical'  => 0,
-            'title_li'      => '<span class="title">' . apply_filters( 'opus_404_top_10_title', __( 'Top 10 Categories by Post Count:', 'opusprimus' ) ) . '</span>',
-            'number'        => 10,
-        ) );
+			/** Display a list of categories to choose from */
+			$opus_archives->categories_archive( array(
+				'orderby'      => 'count',
+				'order'        => 'desc',
+				'show_count'   => 1,
+				'hierarchical' => 0,
+				'title_li'     => '<span class="title">' . apply_filters( 'opus_404_top_10_title', __( 'Top 10 Categories by Post Count:', 'opusprimus' ) ) . '</span>',
+				'number'       => 10,
+			) );
 
-        printf( '<p class="opus-404-tag-text">%1$s</p>',
-            apply_filters( 'opus_404_tag_text', __( '... or maybe you are interested in one of these tags?', 'opusprimus' ) )
-        );
+			printf( '<p class="opus-404-tag-text">%1$s</p>',
+				apply_filters( 'opus_404_tag_text', __( '... or maybe you are interested in one of these tags?', 'opusprimus' ) )
+			);
 
-        /** Display a list of tags to choose from */
-        $opus_archives->archive_cloud( array(
-            'taxonomy'  => 'post_tag',
-            'orderby'   => 'count',
-            'order'     => 'DESC',
-            'number'    => 10,
-        ) ); ?>
+			/** Display a list of tags to choose from */
+			$opus_archives->archive_cloud( array(
+				'taxonomy' => 'post_tag',
+				'orderby'  => 'count',
+				'order'    => 'DESC',
+				'number'   => 10,
+			) ); ?>
 
-    </div><!-- #the-loop -->
+		</div>
+		<!-- #the-loop -->
 
-    <?php
-    /** Add empty action after the_Loop */
-    do_action( 'opus_the_loop_after' );
+		<?php
+		/** Add empty action after the_Loop */
+		do_action( 'opus_the_loop_after' );
 
-    get_sidebar( '404' );
+		get_sidebar( '404' );
 
-    /** Close the classes written by the layout_open call */
-    echo $opus_structures->layout_close();
+		/** Close the classes written by the layout_open call */
+		echo $opus_structures->layout_close();
 
-    /** Add empty hook at the bottom of the content */
-    do_action( 'opus_content_bottom' ); ?>
+		/** Add empty hook at the bottom of the content */
+		do_action( 'opus_content_bottom' ); ?>
 
-</div><!-- #content-wrapper -->
+	</div><!-- #content-wrapper -->
 
 <?php
 /** Add empty hook after the content */
