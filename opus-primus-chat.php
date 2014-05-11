0b10276b4ec03b79bc3666a5f232f07d21bb6bb1
Edward Caissie <edward.caissie@gmail.com>
2014-05-11 13:19:24 -0400

    - added featured image thumbnail to Chat post-format

--- opus-primus-chat.php
+++ opus-primus-chat.php
@@ -34,6 +34,10 @@
  * @version     1.2
  * @date        April 20, 2013
  * Adjusted conditional displaying the `featured_image`
+ *
+ * @version     1.2.4
+ * @date        May 11, 2014
+ * Use `show_featured_thumbnail` method
  */
 
 /** Call the class variables */
@@ -54,15 +58,8 @@ global $opus_defaults, $opus_posts, $opus_comments, $opus_navigation, $opus_imag
 				   )
 		);
 		$opus_posts->post_title();
-
-		if ( ! is_single() ) {
-			$opus_comments->comments_link();
-			$opus_images->featured_thumbnail();
-		} else {
-			$opus_images->featured_thumbnail( $size = 'full', $class = 'aligncenter' );
-		}
-		/** End if - not is single */
-
+		$opus_comments->comments_link();
+		$opus_images->show_featured_thumbnail();
 		$opus_posts->post_content();
 		$opus_navigation->multiple_pages_link( array(), $preface = __( 'Pages:', 'opus-primus' ) );
 		$opus_posts->meta_tags( $anchor );
